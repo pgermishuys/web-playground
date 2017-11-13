11846b7d16b79b861f2fa79d01c0752b41af765e
Pieter Germishuys <pieter@pieterg.com>
2017-11-13 22:33:32 +0100

    Add redux

--- client/index.js
+++ client/index.js
@@ -2,13 +2,18 @@ import React from 'react'
 import { render } from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 import App from '../common/App'
-render(<AppContainer>
- <App />
-</AppContainer>, document.getElementById('root'))
+
+render(
+    <AppContainer>
+        <App />
+    </AppContainer>, document.getElementById('root')
+)
 if (module.hot) {
     module.hot.accept('../common/App', () => {
-        render(<AppContainer>
-          <App />
-        </AppContainer>, document.getElementById('root'))
+        render(
+            <AppContainer>
+                <App />
+            </AppContainer>, document.getElementById('root')
+        )
     })
 }
